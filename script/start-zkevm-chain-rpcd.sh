e3ad64e6756bec0e224760a51cb3dad746bc2957
luanxu <dmail.yui@gmail.com>
2023-04-07 01:54:10 +0800

    feat(script): prover keys file

--- script/start-zkevm-chain-rpcd.sh
+++ script/start-zkevm-chain-rpcd.sh
@@ -6,11 +6,11 @@ if [ "$ENABLE_PROVER" == "true" ]; then
     mkdir -p /data
 
     if [ ! -f "/data/19.bin" ];then
-        wget https://storage.cloud.google.com/mxc-zkevm-circuits-keys/19.bin -P /data
+        wget https://storage.googleapis.com/mxc-zkevm-circuits-keys/19.bin -P /data
     fi
 
     if [ ! -f "/data/21.bin" ];then
-        wget https://storage.cloud.google.com/mxc-zkevm-circuits-keys/21.bin -P /data
+        wget https://storage.googleapis.com/mxc-zkevm-circuits-keys/21.bin -P /data
     fi
 
     /prover_rpcd --bind 0.0.0.0:${PORT_ZKEVM_CHAIN_PROVER_RPCD}

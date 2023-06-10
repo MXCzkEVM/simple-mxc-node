ba573053eacba2900d78272b5a8c2b582bc4d507
luanxu-mxc <luanxu@mxc.org>
2023-06-10 11:30:24 +0800

    feat: mxc-wannsee-alpha3

--- script/start-proposer.sh
+++ script/start-proposer.sh
@@ -5,14 +5,14 @@ set -eou pipefail
 if [ "$ENABLE_PROPOSER" == "true" ]; then
     mxc-client proposer \
       --l1.ws ${L1_ENDPOINT_WS} \
+      --l1.http ${L1_ENDPOINT_HTTP} \
       --l2.http http://l2_execution_engine:8545 \
       --mxcL1 ${MXC_L1_ADDRESS} \
       --mxcL2 ${MXC_L2_ADDRESS} \
       --l1.proposerPrivKey ${L1_PROPOSER_PRIVATE_KEY} \
       --l2.suggestedFeeRecipient ${L2_SUGGESTED_FEE_RECIPIENT} \
-      --proposeInterval "3s" \
-      --proposeEmptyBlockInterval "4h" \
-      --txMinGasPrice "3000000000000" \
+      --proposeInterval "20s" \
+      --proposeEmptyBlockInterval "4h"
 else
     sleep infinity
 fi
